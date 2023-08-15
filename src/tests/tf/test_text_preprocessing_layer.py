63ad378abdadbdb0f33b3b3aef4725c54d075879
Tor Björgen <tor.bjorgen@scilifelab.se>
2023-08-15 11:55:43 +0200

    Text preprocessing layer: Support multitensor input
    Signed-off-by: Tor Björgen <tor.bjorgen@scilifelab.se>

--- src/tests/tf/test_text_preprocessing_layer.py
+++ src/tests/tf/test_text_preprocessing_layer.py
@@ -4,10 +4,14 @@ from rdds.lib.tf import TextPreprocessingLayer
 
 
 def test_preprocessing_layer():
-
+    """
+    Test text preprocessing layer splitting functionality.
+    """
+    # GIVEN a text preprocessing layer
     preprocessing_layer = TextPreprocessingLayer()
-
+    # WHEN processing the data
     data = preprocessing_layer(tf.constant('foo bar')).numpy()
+    # THEN expect data to be appropriately split
     assert data[0][0] == b'foo'
     assert data[0][1] == b'bar'
 
@@ -18,3 +22,18 @@ def test_preprocessing_layer():
     data = preprocessing_layer(tf.constant('a\nb')).numpy()
     assert data[0][0] == b'a'
     assert data[0][1] == b'b'
+
+
+def test_preprocessing_layer_multi_tensor():
+    """
+    Test for providing multiple tensors as input to TextPreprocessingLayer.
+    """
+    # GIVEN a preprocessing layer
+    preprocessing_layer: TextPreprocessingLayer = TextPreprocessingLayer()
+    # WHEN providing multiple tensors as input
+    t0, t1 = preprocessing_layer(tf.constant(['foo bar']), tf.constant('lol cat'))
+    # THEN expect individual tensors back, each processed
+    assert t0.shape == (1, None)
+    assert t0.numpy()[0][0] == b'foo'
+    assert t1.shape == (1, None)
+    assert t1.numpy()[0][1] == b'cat'
