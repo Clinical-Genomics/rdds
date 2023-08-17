3eda7a12c1c4ed55dec102532d38701f3c1818e8
Tor Björgen <tor.bjorgen@scilifelab.se>
2023-08-17 10:02:56 +0200

    Exploration rankscore: Use workdir path module
    Signed-off-by: Tor Björgen <tor.bjorgen@scilifelab.se>

--- src/rdds/exploration_rankscore/__init__.py
+++ src/rdds/exploration_rankscore/__init__.py
@@ -1,2 +1,2 @@
-from os.path import abspath, dirname
-WORKDIR = abspath(dirname(__file__)+'../../../../tmp/exploration-rankscore')
+from rdds.lib.workdir import get_workdir_path
+WORKDIR = get_workdir_path('exploration-rankscore')
