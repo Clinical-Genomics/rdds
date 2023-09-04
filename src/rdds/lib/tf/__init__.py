e626de258862138b583ad65b995fa6cc694ddf08
Tor Björgen <tor.bjorgen@scilifelab.se>
2023-09-04 16:12:12 +0200

    Add TF DnaSequenceTrimmer
    This preprocessing layer removes DNA sequences from
    the input tensor.
    
    Signed-off-by: Tor Björgen <tor.bjorgen@scilifelab.se>

--- src/rdds/lib/tf/__init__.py
+++ src/rdds/lib/tf/__init__.py
@@ -2,3 +2,4 @@ from .data_generator import get_tf_dataset_from_hd5_data_generator
 from .text_preprocessing_layer import TextPreprocessingLayer
 from .text_vectorization_layer import TextVectorizationLayer
 from .tfprint import tfprint
+from .dna_sequence_trimmer import DnaSequenceTrimmer
