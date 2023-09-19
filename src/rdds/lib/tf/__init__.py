7b5ff7b0175900354d0a98b3161f53ff7282ba89
Tor Björgen <tor.bjorgen@scilifelab.se>
2023-09-19 10:18:45 +0200

    HD5 generator, TF Dataset: Label sampling
    Allow sampling of categorical labels for equally balanced training set.
    
    Signed-off-by: Tor Björgen <tor.bjorgen@scilifelab.se>

--- src/rdds/lib/tf/__init__.py
+++ src/rdds/lib/tf/__init__.py
@@ -3,3 +3,4 @@ from .text_preprocessing_layer import TextPreprocessingLayer
 from .text_vectorization_layer import TextVectorizationLayer
 from .tfprint import tfprint
 from .dna_sequence_trimmer import DnaSequenceTrimmer
+from .dataset_rejection_resample import rejection_resample
