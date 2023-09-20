a06d32f0e3c2b0a0de00805969bcd4ae8a240782
Tor Björgen <tor.bjorgen@scilifelab.se>
2023-09-20 10:03:34 +0200

    RankScore test: Update wrapper shellscript with new data paths

--- src/rdds/exploration_rankscore/rankscore-data-analysis.sh
+++ src/rdds/exploration_rankscore/rankscore-data-analysis.sh
@@ -7,26 +7,27 @@ set -e
 . /opt/conda/bin/activate
 
 export PYTHONPATH=/rdds/src
-export DDIR=/rdds/tmp/rankscore-eval
+export DDIR=/rdds/tmp/exploration-rankscore/data
 export LOGDIR=/rdds/tmp/exploration-rankscore
 export PYTHONUNBUFFERED=1
 # NOTE to self: Done use genmod score here, reference genmod file is from _compound step.
-export VCF=$DDIR/snv_indel/genmod_compound/mutacc-20230512_gatkcomb_rhocall_norm_af_mt_frqf_cadd_vep_parsed.annotate_models_score_compund.vcf
-export VCF_SV=$DDIR/sv/genmod_compound/mutacc-20230512_comb_ann_vep_parsed.annotate_models_score_compound.vcf
-export VCF_REF=$DDIR/genmod_v3.7.3/mutacc-20230512_gatkcomb_rhocall_norm_af_mt_frqf_cadd_vep_parsed_ranked.vcf.gz
+export VCF=$DDIR/normalized_pr/snv_indel/genmod_compound/mutacc-20230512_gatkcomb_rhocall_norm_af_mt_frqf_cadd_vep_parsed.annotate_models_score_compound.vcf
+export VCF_SV=$DDIR/normalized_pr/sv/genmod_compound/mutacc-20230512_comb_ann_vep_parsed.annotate_models_score_compound.vcf
+export VCF_REF=$DDIR/genmod_v3.7.3/mutacc-20230512_gatkcomb_rhocall_norm_af_mt_frqf_cadd_vep_parsed_ranked.vcf
+export MUTACC_TRUTH=$DDIR/mutacc-20230512_truth/causative-variants.vcf
 
 # Compile datasets
-python3 -m rdds.exploration_rankscore compile --vcf $VCF --dataset-file-name `basename $VCF`-snv.hd5 --features RankScore,RankScoreNormalized,RankScoreMinMax --vcf-mutacc-tp-cases /rdds/tmp/rankscore-eval/mutacc-20230512_truth/causative-variants.vcf.gz >$LOGDIR/dset.log 2>&1 &
-python3 -m rdds.exploration_rankscore compile --vcf $VCF_SV --dataset-file-name `basename $VCF_SV`-sv.hd5 --features RankScore,RankScoreNormalized,RankScoreMinMax --vcf-mutacc-tp-cases /rdds/tmp/rankscore-eval/mutacc-20230512_truth/causative-variants.vcf.gz >$LOGDIR/dset_sv.log 2>&1 &
+python3 -m rdds.exploration_rankscore compile --vcf $VCF --dataset-file-name `basename $VCF`-snv.hd5 --features RankScore,RankScoreNormalized,RankScoreMinMax --vcf-mutacc-tp-cases $MUTACC_TRUTH >$LOGDIR/dset.log 2>&1 &
+python3 -m rdds.exploration_rankscore compile --vcf $VCF_SV --dataset-file-name `basename $VCF_SV`-sv.hd5 --features RankScore,RankScoreNormalized,RankScoreMinMax --vcf-mutacc-tp-cases $MUTACC_TRUTH >$LOGDIR/dset_sv.log 2>&1 &
 python3 -m rdds.exploration_rankscore compile --vcf $VCF_REF --dataset-file-name `basename $VCF_REF`-ref.hd5 --features RankScore >$LOGDIR/dsetref.log 2>&1 &
 
 disown -a
 exit 0
 
 # Run analysis
-export HD5_SNV=/rdds/tmp/exploration-rankscore/mutacc-20230512_gatkcomb_rhocall_norm_af_mt_frqf_cadd_vep_parsed.annotate_models_score_compund.vcf-snv.hd5
-export HD5_SV=/rdds/tmp/exploration-rankscore/mutacc-20230512_comb_ann_vep_parsed.annotate_models_score_compound.vcf-sv.hd5
-export HD5_REF=/rdds/tmp/exploration-rankscore/mutacc-20230512_gatkcomb_rhocall_norm_af_mt_frqf_cadd_vep_parsed_ranked.vcf.gz-ref.hd5
+export HD5_SNV=$LOGDIR/mutacc-20230512_gatkcomb_rhocall_norm_af_mt_frqf_cadd_vep_parsed.annotate_models_score_compound.vcf-snv.hd5
+export HD5_SV=$LOGDIR/mutacc-20230512_comb_ann_vep_parsed.annotate_models_score_compound.vcf-sv.hd5
+export HD5_REF=$LOGDIR/mutacc-20230512_gatkcomb_rhocall_norm_af_mt_frqf_cadd_vep_parsed_ranked.vcf-ref.hd5
 
 python3 -m rdds.exploration_rankscore testnormalizedrankscore --hd5 $HD5_SNV --hd5ref $HD5_REF > $LOGDIR/testnormalizedrankscore-snv.log 2>&1 &
 python3 -m rdds.exploration_rankscore testnormalizedrankscore --hd5 $HD5_SV --hd5ref $HD5_REF > $LOGDIR/testnormalizedrankscore-sv.log 2>&1 &
