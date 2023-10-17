0f6cffedab717eb1285506b0532fd45a26d32597
Tor Björgen <tor.bjorgen@scilifelab.se>
2023-10-17 18:45:58 +0200

    Print environ on test

--- build/devenv/gputest.py
+++ build/devenv/gputest.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
+from os import environ
 from tensorflow.python.platform import build_info as build
+
+print(environ)
 
 print(f"TF was built with CUDA v{build.build_info['cuda_version']}")
 
