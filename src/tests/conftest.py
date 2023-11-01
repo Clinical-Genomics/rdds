fadc24fee25a58dad2a7319a64f66487ba9247f2
Tor Björgen <tor.bjorgen@scilifelab.se>
2023-11-01 11:39:27 +0100

    conftest: Import hd5 data generator fixtures
    Signed-off-by: Tor Björgen <tor.bjorgen@scilifelab.se>

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,2 +1,6 @@
 from fixtures.text_vectorization import work_dir, word_dataset
-from fixtures.hdf5_data import hd5_file
+from fixtures.hdf5_data import (
+    hd5_file,
+    hd5_file_path_with_nans,
+    hd5_file_path_with_categorical_labels
+    )
