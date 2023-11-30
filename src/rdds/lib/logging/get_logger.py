1cbc9c798db2c3d16fa468020d647bddb7526901
Tor Björgen <tor.bjorgen@scilifelab.se>
2023-11-30 11:16:31 +0100

    logging: Option to provide log level on logger creation
    Signed-off-by: Tor Björgen <tor.bjorgen@scilifelab.se>

--- src/rdds/lib/logging/get_logger.py
+++ src/rdds/lib/logging/get_logger.py
@@ -1,13 +1,22 @@
-from logging import Logger, basicConfig, getLogger
+from logging import Logger, basicConfig, getLogger, DEBUG, INFO, WARNING, FATAL
 
 from rdds.lib.logging import constants
 
+_LOG_LEVELS = {'debug': DEBUG,
+               'info': INFO,
+               'warning': WARNING,
+               'fatal': FATAL}
 
-def get_logger(name: str = None) -> Logger:
+def get_logger(name: str = None,
+               log_level: str = None) -> Logger:
     """
     Convenience method for configuring logging module.
     :param name: Name of logger
+    :param log_level: Str matching names in logging.[LOG_LEVEL]
     :return: An instance of Logger
     """
     basicConfig(format=constants.LOGGING_FORMAT)
-    return getLogger(name=name)
+    logger: Logger = getLogger(name=name)
+    if log_level:
+        logger.setLevel(_LOG_LEVELS[log_level])
+    return logger
