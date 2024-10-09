bc08e106269dd915fe3f0efb5d14b4e8533b8e15
Tor Björgen <tor.bjorgen@scilifelab.se>
2024-10-09 12:59:00 +0200

    VRS: Embeddings reduction using element wise max
    Signed-off-by: Tor Björgen <tor.bjorgen@scilifelab.se>

--- src/rdds/variant_rank_score/model/model.py
+++ src/rdds/variant_rank_score/model/model.py
@@ -157,8 +157,9 @@ class VariantRankScoreModel:
         # Required because reduce_prod adds 1.0 to empty Ragged dimensions.
         embeddings = embeddings.to_tensor()
 
-        # Reduce dimensions by computing the average of all words per every feature vector
-        embeddings = tf.math.reduce_mean(embeddings, axis=2, keepdims=True)  # -> [bdim, n_features, 1, n_embeddings]
+        # Reduce dimensions by computing the maximum of all words per every feature vector
+        # This is an permutation invariant downsampling method for the embeddings
+        embeddings = tf.math.reduce_max(embeddings, axis=2, keepdims=True)  # -> [bdim, n_features, 1, n_embeddings]
 
         # Flatten word vector to -> [bdim, n_features * n_embeddings]
         embeddings_flat = tf.reshape(embeddings, (-1, len(self._features_text) * self._embedding_dimensions))
