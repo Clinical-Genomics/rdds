f158027d7255e2f1cacaf6467d4e5bdb38c62838
Tor Björgen <tor.bjorgen@scilifelab.se>
2024-11-04 10:56:36 +0100

    VRS: Earlystopping, 3 epochs patience

--- src/rdds/variant_rank_score/model/model.py
+++ src/rdds/variant_rank_score/model/model.py
@@ -500,7 +500,8 @@ class VariantRankScoreModel:
         callbacks.append(tf.keras.callbacks.LambdaCallback(on_epoch_end=self.save_model_fn))
         callbacks.append(tf.keras.callbacks.EarlyStopping(monitor='val_loss',
                                                           mode='min',
-                                                          verbose=1))
+                                                          verbose=1,
+                                                          patience=3))
         callbacks.append(tf.keras.callbacks.TerminateOnNaN())
 
         compile_config: Dict[str, Any] = self._keras_model.get_compile_config()
