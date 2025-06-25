EmbeddingsReductionLayer: Bugfix for embedding leakage across batchDim
The 0.0 padding and reduce_max technique does not play well.
The padding value unintentionally masks embeddings
in conjunction with the max() op when input
dimensionality changes.

Therefore, change the reduction operation
to a method insensitive to padding contribution
which in addition increases the embedding
fidelity.

Signed-off-by: Tor Björgen <tor.bjorgen@scilifelab.se>

src/rdds/lib/tf/embeddings_reduction.py
@@ -23,6 +23,10 @@ class EmbeddingsReductionLayer(TextVectorizationLayer):
         If this is not the case, empty embedding dimensions are filled with unallocated values from RAM.
         [[], ['foo bar']] --> [ [[0, 0], [0, 0]], [[1, 2], [3, 4]] ]
 
+        Note that the `embeddings.to_tensor(default_value=v, ...)` padding value and the reduction technique
+        are interdependent; padding with 0.0 works well with a reduce_sum operation but not
+        well with reduce_prod or reduce_max.
+
         Additionally make sure the output shape has at least one populated word dimension
         [bdim, feature_dim, word_dim, embeddings]
         """
@@ -40,5 +44,5 @@ class EmbeddingsReductionLayer(TextVectorizationLayer):
             zero_padded_embeddings: tf.Tensor = embeddings.to_tensor(default_value=tf.constant(0.0), shape=shape_padded)
 
         # Do the reduction
-        embeddings_dim_reduced = tf.math.reduce_max(zero_padded_embeddings, axis=2, keepdims=True)
+        embeddings_dim_reduced = tf.math.reduce_sum(zero_padded_embeddings, axis=2, keepdims=True)
         return embeddings_dim_reduced