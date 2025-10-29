da0bbfbadf496d850a1a8ab8de84d1e6554512bd
Tor Björgen <tor.bjorgen@scilifelab.se>
2025-10-29 13:02:34 +0100

    vrs statfns: Compute threshold performance in parallel
    Signed-off-by: Tor Björgen <tor.bjorgen@scilifelab.se>

--- src/tests/variant_rank_score/test_statfns.py
+++ src/tests/variant_rank_score/test_statfns.py
@@ -1,17 +1,18 @@
 import numpy as np
 from os.path import join
+import pytest as pt
 
 from rdds.variant_rank_score.inference_exploration.statfns import plot_performance_vs_threshold
 
 
-
-def test_performance_vs_threshold(work_dir):
+@pt.mark.parametrize('n_samples', [20, int(1E6)])
+def test_performance_vs_threshold(work_dir, n_samples):
     """
     Test for configuring plotting behavior
     """
-    v = np.linspace(0, 1, 20)
-    l = np.zeros(20)
-    l[10:] = 1
+    v = np.linspace(0, 1, n_samples)
+    l = np.zeros_like(v)
+    l[int(n_samples/2):] = 1
     plot_performance_vs_threshold(predictions=v,
                                   labels=l,
                                   output_path=join(work_dir, 'p.png'))
