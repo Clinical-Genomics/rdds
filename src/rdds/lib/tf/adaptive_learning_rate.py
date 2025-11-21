af647327b47f0ffa032dd8c8970df758741e99ea
Tor Björgen <tor.bjorgen@scilifelab.se>
2025-11-21 16:31:30 +0100

    fixup! lib/tf: Use LearningRateScheduler for adaptive learning rate

--- src/rdds/lib/tf/adaptive_learning_rate.py
+++ src/rdds/lib/tf/adaptive_learning_rate.py
@@ -1,8 +1,10 @@
 from tensorflow.keras.callbacks import LearningRateScheduler
+from tensorflow.summary import SummaryWriter, scalar as tf_summary_scalar
 
 def _adaptive_learning_rate(network_param: int,
                            epoch_number: int,
-                           warmup_epochs: int):
+                           warmup_epochs: int,
+                           writer: SummaryWriter = None):
     """
     Adaptive learning rate.
 
@@ -19,18 +21,24 @@ def _adaptive_learning_rate(network_param: int,
 
     lr = (network_param ** -0.5) * \
          min(epoch_number ** -0.5, epoch_number * warmup_epochs ** -1.5)
-    return lr
 
+    if writer:
+        with writer.as_default():
+            tf_summary_scalar('learning_rate', lr, step=epoch_number)
+
+    return lr
 
 class AdaptiveLearningRate(LearningRateScheduler):
 
-    def __init__(self, network_param: int, warmup_epochs: int, verbose: int = 1):
+    def __init__(self, network_param: int, warmup_epochs: int, verbose: int = 1, writer: SummaryWriter = None):
 
         self._network_param = network_param
         self._warmup_epochs = warmup_epochs
+        self._writer = writer
         self._fn = lambda epoch_index, current_learning_rate: _adaptive_learning_rate(network_param=self._network_param,
                                                                                       epoch_number=epoch_index,
-                                                                                      warmup_epochs=self._warmup_epochs)
+                                                                                      warmup_epochs=self._warmup_epochs,
+                                                                                      writer=self._writer)
 
         super().__init__(schedule=self._fn,
                          verbose=verbose)
