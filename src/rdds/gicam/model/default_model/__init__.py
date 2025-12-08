eeb3472175cbad0ded02e1dc1d8a7607251b4d54
Tor Björgen <tor.bjorgen@scilifelab.se>
2025-12-08 11:07:54 +0100

    gicam: Update GICAM to model without MTAF annotations
    Signed-off-by: Tor Björgen <tor.bjorgen@scilifelab.se>

--- src/rdds/gicam/model/default_model/__init__.py
+++ src/rdds/gicam/model/default_model/__init__.py
@@ -7,9 +7,9 @@ class ModelFileSpec:
     model_version: str
     keras_model: str
 
-DEFAULT_MODEL_VERSION = "20251021-203805-gicam-improvement-rc4"
+DEFAULT_MODEL_VERSION = "v1.12.0-rc4-21-g55bcfff"
 DEFAULT_MODEL_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), DEFAULT_MODEL_VERSION))
-DEFAULT_KERAS_MODEL_FILE = os.path.join(DEFAULT_MODEL_DIRECTORY, 'saved-models', '50-0.0005.keras')
+DEFAULT_KERAS_MODEL_FILE = os.path.join(DEFAULT_MODEL_DIRECTORY, 'saved-models', '70-0.0006.keras')
 DEFAULT_MODEL_SPEC = ModelFileSpec(
     model_version=DEFAULT_MODEL_VERSION,
     keras_model=DEFAULT_KERAS_MODEL_FILE
