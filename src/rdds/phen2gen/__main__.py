8402edbfc71520c2acca28d5d60317d1809edc1b
Tor Björgen <tor.bjorgen@scilifelab.se>
2026-02-17 10:18:33 +0100

    fixup! Add phen2gen module#

--- src/rdds/phen2gen/__main__.py
+++ src/rdds/phen2gen/__main__.py
@@ -9,5 +9,14 @@ subparsers = parser.add_subparsers()
 subparser = subparsers.add_parser('compile-dataset', help='Build dataset for training')
 subparser.set_defaults(func=lambda args: Phen2GenDatasetCompiler().compile())
 
+subparsers = parser.add_subparsers()
+subparser = subparsers.add_parser('build-train', help='Build dataset for training and train')
+subparser.add_argument('--vcf', default='/rdds/tmp/justhusky_short.vcf', help='Path to VCF file')  # FIXME default
+def build_train(args):
+    from .model.model import GnnModel
+    model = GnnModel()
+    model.build_dataset(vcf_path=args.vcf_path)
+subparser.set_defaults(func=lambda args: build_train(args))
+
 args = parser.parse_args()
 args.func(args)  # Callback to trigger func with args
